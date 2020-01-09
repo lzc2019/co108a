89614b35b8ad8b235a42b53932d418d781ef55b7
alen19990107 <alen19990107@gmail.com>
2020-01-09 09:05:00 +0800

    03

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,58 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(FOREVER)
+// n=8192
+	@8192
+	D=A
+	@n
+	M=D
+// i = 0
+	@i
+	M=0
+(LOOP)
+  // if (i==n) goto ENDLOOP
+	@i
+	D=M
+	@n
+	D=D-M
+	@ENDLOOP
+	D; JEQ
+	
+  // if (*KBD != 0)
+	@KBD
+	D=M     // D = *KBD
+	@ELSE
+	D; JEQ  // if (*KDB==0) goto ELSE
+	
+	//   RAM[SCREEN+i] = -1
+	@SCREEN
+	D=A
+	@i
+	A=D+M
+	M=-1
+	
+	@ENDIF
+	0; JMP
+(ELSE)	
+  // else 
+  //   RAM[SCREEN+i] = 0
+	@SCREEN
+	D=A
+	@i
+	A=D+M
+	M=0
+	
+(ENDIF)
+	
+	// i++
+	@i
+	M=M+1
+	
+	@LOOP
+	0; JMP
+
+(ENDLOOP)
+	@FOREVER
+	0; JMP
